bedd50068b6110af96327ee4b9876a19758a3471
cth90 <hollyca@mail.gvsu.edu>
2021-09-29 20:18:35 -0400

    Delete adopt-a-hemlock directory

--- adopt-a-hemlock/adopt-a-hemlock.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
-* Plugin Name: Adopt-A-Hemlock
-* Plugin URI: https://github.com/cth90/CIS467-Team-Hemlock
-* Description: Plugin for the Adopt-A-Hemlock Fundraising Campaign
-* Version: 0.1
-* Author: Cameron Holly, Mike Day, Brandon Moody
-**/
