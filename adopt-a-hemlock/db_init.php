9d96d001e9e6bad3a96c67392d49a525e9fa4b3e
cth90 <hollyca@mail.gvsu.edu>
2021-09-29 20:17:54 -0400

    added function to create tables

--- adopt-a-hemlock/db_init.php
+++ adopt-a-hemlock/db_init.php
@@ -1,8 +1,26 @@
 <?php
 /**
  * This file contains the code for initializing the database for the plugin
- * using the same db as Wordpress.
+ * using the same db as WordPress.
  */
 
-global $wpdb;
+function create_tables($sql_file) {
 
+    // global variable for wordpress db class
+    global $wpdb;
+
+    // load file
+    $queries = file_get_contents($sql_file);
+
+    // split into array
+    $queries = explode(";", $queries);
+
+    // run queries
+    foreach ($queries as $query) {
+        if ( !( $wpdb->query($query) ) ) {
+            // if a query fails
+            echo "<div><h1>Query failed:</h1><br><p>$query</p></div>";
+            break;
+        }
+    }
+}
