d634f5d91fd1f95545c3bec02fcafd62b01727af
cth90 <hollyca@mail.gvsu.edu>
2021-09-30 14:52:45 -0400

    Cleaning up code

--- adopt-a-hemlock/settings.php
+++ adopt-a-hemlock/settings.php
@@ -12,7 +12,7 @@ function aah_create_tables_action_ajax() {
     $result = aah_create_tables(sql_file);
     wp_send_json($result);
 }
-// Add the ajax action
+// Add the hook for the ajax action
 add_action('wp_ajax_aah_ajax_create_tables', 'aah_create_tables_action_ajax');
 
 // This adds the menu page for our plugin
@@ -20,7 +20,6 @@ function aah_configure_settings_page() {
     add_menu_page( "Adopt-a-Hemlock Settings", "Adopt-a-Hemlock Settings",
         "manage_options", "adopt-a-hemlock", "aah_render_tables_create_page");
 }
-
 // hook to add settings to admin menu
 add_action( 'admin_menu', 'aah_configure_settings_page' );
 
@@ -30,10 +29,6 @@ function aah_render_tables_create_page() {
     <div class="aah-create-tables">
     <h2>Create Database Tables</h2>
     <button class="aah-create-tables-button">Create Tables</button>
-<!--    <form action="--><?php //echo admin_url( 'admin-post.php' ); ?><!--" method="post">-->
-<!--        <input type="hidden" name="action" value="aah_create_tables">-->
-<!--        --><?php //submit_button( 'Create' ); ?>
-<!--    </form>-->
     <script type="text/javascript">
         jQuery(document).ready(function($) {
             $('.aah-create-tables-button').click( function (event) {
