088193a29529812e7a59049c036310fb84786b67
cth90 <hollyca@mail.gvsu.edu>
2021-10-07 19:38:56 -0400

    updated sql queries to include locations

--- adopt-a-hemlock/database_structure.sql
+++ adopt-a-hemlock/database_structure.sql
@@ -35,3 +35,12 @@ ALTER TABLE `aah_transactions` ADD FOREIGN KEY (`customer_id`) REFERENCES `aah_c
 ALTER TABLE `aah_transactions` ADD FOREIGN KEY (`tree_id`) REFERENCES `aah_trees` (`tag`);
 
 ALTER TABLE `aah_trees` ADD FOREIGN KEY (`location_id`) REFERENCES `aah_locations` (`id`);
+
+INSERT INTO aah_locations (name, parcel, address)
+VALUES ('Duncan Woods', '70-03-29-200-010', 'Grand Haven, MI 49417');
+
+INSERT INTO aah_locations (name, parcel, address)
+VALUES ('Cemetery', '70-03-29-325-016', '1304 Lake Ave Cemetery, Grand Haven, MI 49417');
+
+INSERT INTO aah_locations (name, parcel, address)
+VALUES ('Mulligan''s Hollow', '70-03-29-160-004', '600 Mulligans Hollow Dr, Grand Haven, MI 49417');
