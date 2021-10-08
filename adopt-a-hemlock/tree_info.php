f54e40f3e2b9a2a8305b72f58c670bc56eec9b7a
cth90 <hollyca@mail.gvsu.edu>
2021-10-08 15:40:25 -0400

    added functions to get any unadopted tree, get all adopted trees, and create a dummy transaction to adopt a tree for dev purposes

--- adopt-a-hemlock/tree_info.php
+++ adopt-a-hemlock/tree_info.php
@@ -30,16 +30,22 @@ function aah_get_all_adopted_trees()
 // Create a dummy transaction to adopt a tree by its id (not tag) for development purposes
 function aah_adopt_tree_by_id($tree_id)
 {
+    global $wpdb;
     $transaction_info = array(
         'name' => 'Nyatasha Nyanners',
         'payment_id' => 9999,
-        'amt_donated' => '99,999,999.99',
+        'amt_donated' => '99999999.99',
         'anonymous' => 'false',
         'tree_id' => $tree_id
     );
     $query = 'INSERT INTO `aah_transactions` (name, payment_id, amt_donated, tree_id, anonymous)
-SELECT %s, %d, %s, id, %s
-FROM `aah_trees`
-WHERE id = %s
-AND id NOT IN (SELECT b.tree_id FROM `aah_transactions` b)';
+SELECT %s, %d, %s, b.id, %s
+FROM `aah_trees` b
+WHERE b.id = %d
+AND b.id NOT IN (SELECT c.tree_id FROM `aah_transactions` c)';
+    $result = $wpdb->query($wpdb->prepare($query, $transaction_info));
+    if (!$result) {
+        trigger_error("Transaction not created; tree already adopted or does not exist.");
+    }
+    return $result;
 }
