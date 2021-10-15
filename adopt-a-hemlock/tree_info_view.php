5231c71eddf29b6339e831053eea1b86a8528c4b
cth90 <hollyca@mail.gvsu.edu>
2021-10-14 20:11:45 -0400

    debugging ajax form for tree lookup

--- adopt-a-hemlock/tree_info_view.php
+++ adopt-a-hemlock/tree_info_view.php
@@ -11,14 +11,14 @@ function aah_render_tree_lookup()
     ob_start();
     ?>
     <div class="tree-lookup">
-        <form action="" method="post" name="tree-lookup-form">
+        <form action="" method="post" class="tree-lookup-form">
             <div class="form-field">
                 <label>Tag: </label>
                 <input name="tree_tag" type="text">
             </div>
             <input type="hidden" name="action" value="aah_get_tree_info">
-            <button type="button">Lookup Tree</button>
         </form>
+        <button type="button" class="tree-lookup-button">Lookup Tree</button>
     </div>
     <div class="tree-info" style="display:none">
         <label>ID: </label><div class="tree-id"></div><br>
@@ -31,18 +31,20 @@ function aah_render_tree_lookup()
 
     </div>
     <script type="text/javascript">
-        jQuery('form[name="tree-lookup-form"]').on('button', function () {
-            var form_data = jQuery(this).serializeArray();
-            $.post(ajaxurl, form_data, function (response) {
-                var div = $(".tree-info");
-                $(div).find(".tree-id").html(response['id']);
-                $(div).find(".tree-tag").html(response['tag']);
-                $(div).find(".tree-dbh").html(response['dbh']);
-                $(div).find(".tree-lat").html(response['latitude']);
-                $(div).find(".tree-long").html(response['longitude']);
-                $(div).find(".tree-loc").html(response['location_id']);
-                $(div).find(".tree-notes").html(response['notes']);
-                $(div).show();
+        jQuery(document).ready(function($){
+            $('.tree-lookup-button').click(function() {
+                var form_data = jQuery('.tree-lookup-form').serializeArray();
+                $.post(ajaxurl, form_data, function (response) {
+                    var div = $(".tree-info");
+                    $(div).find(".tree-id").html(response['id']);
+                    $(div).find(".tree-tag").html(response['tag']);
+                    $(div).find(".tree-dbh").html(response['dbh']);
+                    $(div).find(".tree-lat").html(response['latitude']);
+                    $(div).find(".tree-long").html(response['longitude']);
+                    $(div).find(".tree-loc").html(response['location_id']);
+                    $(div).find(".tree-notes").html(response['notes']);
+                    $(div).show();
+                });
             });
         });
     </script>
