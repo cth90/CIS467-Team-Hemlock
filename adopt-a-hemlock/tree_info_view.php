67f6be86f2af5fab9877fefd7578c6d84df13880
cth90 <hollyca@mail.gvsu.edu>
2021-10-14 20:34:48 -0400

    changing html style of info display

--- adopt-a-hemlock/tree_info_view.php
+++ adopt-a-hemlock/tree_info_view.php
@@ -21,13 +21,13 @@ function aah_render_tree_lookup()
         <button type="button" class="tree-lookup-button">Lookup Tree</button>
     </div>
     <div class="tree-info" style="display:none">
-        <label>ID: </label><div class="tree-id"></div><br>
-        <label>Tag: </label><div class="tree-tag"></div><br>
-        <label>DBH: </label><div class="tree-dbh"></div><br>
-        <label>Lat: </label><div class="tree-lat"></div><br>
-        <label>Long: </label><div class="tree-long"></div><br>
-        <label>Loc: </label><div class="tree-loc"></div><br>
-        <label>Notes: </label><div class="tree-notes"></div><br>
+        <label>ID: </label><div style="display:inline" class="tree-id"></div><br>
+        <label>Tag: </label><div style="display:inline"  class="tree-tag"></div><br>
+        <label>DBH: </label><div style="display:inline"  class="tree-dbh"></div><br>
+        <label>Lat: </label><div style="display:inline"  class="tree-lat"></div><br>
+        <label>Long: </label><div style="display:inline"  class="tree-long"></div><br>
+        <label>Loc: </label><div style="display:inline"  class="tree-loc"></div><br>
+        <label>Notes: </label><div style="display:inline"  class="tree-notes"></div><br>
 
     </div>
     <script type="text/javascript">
