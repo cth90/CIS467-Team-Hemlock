e285b692b92a6e7173425f19c2fac1a0f51bdcad
cth90 <hollyca@mail.gvsu.edu>
2021-10-27 14:35:43 -0400

    removed javascript from ipn action

--- adopt-a-hemlock/paypal_handler.php
+++ adopt-a-hemlock/paypal_handler.php
@@ -8,15 +8,6 @@ require_once("tree_info.php");
 function aah_paypal_ipn() {
     $id = aah_get_any_unadopted_tree()['id'];
     $result = aah_adopt_tree_by_id($id);
-    ?>
-    <script type="text/javascript">
-        <?php
-            echo "var tree_id = '$id';\n";
-            echo "var result = '$result';\n";
-        ?>
-        alert("Donation received. Tree ID: " . id . " and result: " . result);
-    </script>
-    <?php
 }
 add_action('admin_post_aah_donation_ipn', 'aah_paypal_ipn');
 add_action('admin_post_nopriv_aah_donation_ipn', 'aah_paypal_ipn');
