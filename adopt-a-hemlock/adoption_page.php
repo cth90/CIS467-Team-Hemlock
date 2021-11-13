c7ae955fa8b951032e43396daf729e86657899bb
cth90 <hollyca@mail.gvsu.edu>
2021-11-12 20:49:14 -0500

    working on adoption page

--- adopt-a-hemlock/adoption_page.php
+++ adopt-a-hemlock/adoption_page.php
@@ -10,24 +10,30 @@ function aah_render_adoption_page()
         // todo handle form
     }
 
-
-    $transaction = aah_get_transaction_info_by_aid($_POST['id']);
     ob_start();
-    if (!empty($transaction['tree_tag'])) {
-    ?>
+    if ($transaction = aah_get_transaction_info_by_aid($_POST['id'])) {
+        if (!empty($transaction['tree_tag'])) {
+            ?>
             <div class="adoption-info">
-                <label>Tree Tag: <input name="tree_tag" value="<?php echo $transaction['tree_tag'] ?>" type="text" disabled></label>
-                <label>Location: <input name="tree_location" value="<?php echo $transaction['location_name'] ?>" type="text" disabled></label>
-                <label>Coordinates: <input name="tree_coords" value="<?php echo $transaction['latitude'] . ", " . $transaction['longitude'] ?>" type="text" disabled></label>
-                <label>Notes: <input name="tree_notes" value="<?php echo $transaction['notes'] ?>" type="text" disabled></label>
-                <label>PDF Certificate: <?php echo "<a href='" . $transaction['link'] . "'>" . $transaction['link'] . "</a>"; ?></label>
+                <label>Tree Tag: <input name="tree_tag" value="<?php echo $transaction['tree_tag'] ?>" type="text"
+                                        disabled></label>
+                <br><label>Location: <input name="tree_location" value="<?php echo $transaction['location_name'] ?>"
+                                            type="text" disabled></label>
+                <br><label>Coordinates: <input name="tree_coords"
+                                               value="<?php echo $transaction['latitude'] . ", " . $transaction['longitude'] ?>"
+                                               type="text" disabled></label>
+                <br><label>Notes: <input name="tree_notes" value="<?php echo $transaction['notes'] ?>" type="text"
+                                         disabled></label>
+                <br><label>PDF
+                    Certificate: <?php echo "<a href='" . $transaction['link'] . "'>" . $transaction['link'] . "</a>"; ?></label>
             </div>
-    <?php
-    } else {
-        $locations = aah_get_locations();
-        ?>
+            <?php
+        } else {
+            $locations = aah_get_locations();
+            ?>
             <div class="tree-selection">
-                <p>You have not yet adopted a tree. Please select a location from the dropdown below (select any if you want a location to be selected for you).</p>
+                <p>You have not yet adopted a tree. Please select a location from the dropdown below (select any if you
+                    want a location to be selected for you).</p>
                 <form method="post" class="tree-selection-form" name="tree-selection-form">
                     <select name="tree-locations" id="tree-locations">
                         <option label="Any" value="any" selected>
@@ -41,24 +47,45 @@ function aah_render_adoption_page()
                     <button type="submit" class="tree-selection-submit">Adopt</button>
                 </form>
             </div>
-        <?php
+            <?php
+        }
+    } else {
+        echo "Transaction not found.";
     }
+
     return ob_get_clean();
 }
 add_shortcode('adopt-tree', 'aah_render_adoption_page');
 
-function aah_get_transaction_info_by_aid($aid): array
+function aah_get_transaction_info_by_aid($aid)
 {
-    // todo get transaction info
-    $info = array(
-        'name'=>'Placeholder Name',
-        'date'=>date('m/d/Y'),
-        'tree_tag'=>'0000',
-        'location_name'=>'Wherever',
-        'longitude'=>'9999999.9999',
-        'latitude'=>'9999999.9999',
-        'notes'=>'',
-        'link'=>''
-    );
-    return $info;
+    global $wpdb;
+
+    $sql = 'SELECT
+	t.`name` AS name,
+	tree.`tag` as tree_tag,
+	loc.`name` as location_name,
+	tree.`longitude` as longitude,
+	tree.`latitude` as latitude,
+    tree.`notes` as notes,
+    t.id as transaction_id,
+    t.adoption_id as id,
+    t.pdf_link as link,
+    t.completed as completed,
+    t.email as email,
+    t.anonymous as anonymous
+FROM
+    `aah_transactions` as t
+inner join `aah_trees` as tree on t.id = tree.tree_id
+inner join `aah_locations` as loc on loc.id = t.location_id
+WHERE 
+    tree.adoption_id = %s';
+
+    if (!($result = $wpdb->get_row($wpdb->prepare($sql, $aid), ARRAY_A))) {
+        trigger_error("No matching transaction found.");
+        return false;
+    }
+
+    return $result;
+
 }
