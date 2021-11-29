7a08692124ca4430d10e0af2a65091eead0f0459
cth90 <hollyca@mail.gvsu.edu>
2021-11-29 00:26:59 -0500

    fixed tree add form

--- adopt-a-hemlock/transaction_info.php
+++ adopt-a-hemlock/transaction_info.php
@@ -6,93 +6,100 @@ function aah_render_transaction_lookup()
 {
     ?>
     <table>
-    <tr>
-    <td>
-    <h2>Search Transactions</h2>
-    <p>You can search by adoption ID or tree tag.</p>
-    <div class="transaction-lookup">
-        <form action="" method="post" class="transaction-lookup-form">
-            <div class="form-field">
-                <input name="search-key" type="text"><br>
-                <label>Tree Tag <input type="radio" id="tag-search-type" name="search-type" value="tree-tag"
-                                       checked></label>
-                <label>Adoption ID <input type="radio" id="id-search-type" name="search-type" value="aid"></label><br>
-                <button type="submit" class="transaction-lookup-button">Search</button>
-            </div>
-        </form>
-    </div><br>
+        <tr>
+            <td>
+                <h2>Search Transactions</h2>
+                <p>You can search by adoption ID or tree tag.</p>
+                <div class="transaction-lookup">
+                    <form action="" method="post" class="transaction-lookup-form">
+                        <div class="form-field">
+                            <input name="search-key" type="text"><br>
+                            <label>Tree Tag <input type="radio" id="tag-search-type" name="search-type" value="tree-tag"
+                                                   checked></label>
+                            <label>Adoption ID <input type="radio" id="id-search-type" name="search-type"
+                                                      value="aid"></label><br>
+                            <button type="submit" class="transaction-lookup-button">Search</button>
+                        </div>
+                    </form>
+                </div>
+                <br>
 
-    <?php
-    // if the search was submitted
-    if (isset($_POST['search-key']) and $_POST['search-type']) {
-        if ($_POST['search-type'] == 'aid')
-            $transaction = aah_get_transaction_info_by_aid($_POST['search-key']);
-        else {
-            $transaction = aah_get_transaction_info_by_tag($_POST['search-key']);
-        }
+                <?php
+                // if the search was submitted
+                if (isset($_POST['search-key']) and $_POST['search-type']) {
+                    if ($_POST['search-type'] == 'aid')
+                        $transaction = aah_get_transaction_info_by_aid($_POST['search-key']);
+                    else {
+                        $transaction = aah_get_transaction_info_by_tag($_POST['search-key']);
+                    }
 
-        // If transaction was edited
-        if (isset($_POST['edited'])) {
-            $edited_transaction = array();
-            $transaction_id = $_POST['transaction_id'];
-            if (!empty($_POST['tree_tag']) and $_POST['tree_tag'] != $transaction['tree_tag']) {
-                if (!($edited_transaction['tree_id'] = aah_get_unadopted_tree_by_tag($_POST['tree_tag'])['id'])) {
-                    echo "<h2>Reassigned Tree Tag Not Found!</h2>";
-                }
-            }
-            if (!empty($_POST['name']) and $_POST['name'] != $transaction['name']) {
-                $edited_transaction['name'] = $_POST['name'];
-            }
-            if (!empty($_POST['email']) and $_POST['email'] != $transaction['email']) {
-                $edited_transaction['email'] = $_POST['email'];
-            }
-
-            $edited_transaction['anonymous'] = !empty($_POST['anonymous']);
-
-            if (!(aah_edit_transaction($transaction_id, $edited_transaction))) {
-                echo "<br><h2>Transaction Edit Failed!</h2>";
-            } else {
-                echo "<br><h2>Transaction Successfully Edited!</h2>";
-            }
-
-            if ($_POST['search-type'] == 'aid')
-                $transaction = aah_get_transaction_info_by_aid($_POST['search-key']);
-            else {
-                $transaction = aah_get_transaction_info_by_tag($_POST['search-key']);
-            }
-
-        }
-
-        if ($transaction) {
-            ?>
-            <br>
-            <div class="transaction-info-form">
-                <form action="" method="post" class="transaction-edit-form">
-                    <label>Adoption ID: <a
-                                href="<?php echo site_url('adoption-information') . "?a_id=" . $transaction['a_id']; ?>"><?php echo $transaction['a_id']; ?></a></label>
-                    <br><label>Tree Tag: <input name="tree_tag" id="tree_tag"
-                                                value="<?php echo $transaction['tree_tag']; ?>"></label>
-                    <br><label>Name: <input name="name" id="name" value="<?php echo $transaction['name']; ?>"></label>
-                    <br><label>Email: <input name="email" id="email"
-                                             value="<?php echo $transaction['email']; ?>"></label>
-                    <br><label>Anonymous: <input type="checkbox" name="anonymous" id="anonymous" value="1" <?php
-                        if ($transaction['anonymous']) {
-                            echo "checked";
+                    // If transaction was edited
+                    if (isset($_POST['edited'])) {
+                        $edited_transaction = array();
+                        $transaction_id = $_POST['transaction_id'];
+                        if (!empty($_POST['tree_tag']) and $_POST['tree_tag'] != $transaction['tree_tag']) {
+                            if (!($edited_transaction['tree_id'] = aah_get_unadopted_tree_by_tag($_POST['tree_tag'])['id'])) {
+                                echo "<h2>Reassigned Tree Tag Not Found!</h2>";
+                            }
                         }
-                        ?>></label>
-                    <input type="hidden" name="transaction_id" id="transaction_id"
-                           value="<?php echo $transaction['transaction_id']; ?>">
-                    <input type="hidden" name="search-key" id="search-key" value="<?php echo $_POST['search-key']; ?>">
-                    <input type="hidden" name="search-type" id="search-type"
-                           value="<?php echo $_POST['search-type']; ?>">
-                    <input type="hidden" name="edited" id="edited" value="1">
-                    <br>
-                    <button type="submit" class="transaction-edit-button">Submit Edits</button>
-                </form>
-            </div>
-            <?php
-        } ?>
-        </td></tr>
+                        if (!empty($_POST['name']) and $_POST['name'] != $transaction['name']) {
+                            $edited_transaction['name'] = $_POST['name'];
+                        }
+                        if (!empty($_POST['email']) and $_POST['email'] != $transaction['email']) {
+                            $edited_transaction['email'] = $_POST['email'];
+                        }
+
+                        $edited_transaction['anonymous'] = !empty($_POST['anonymous']);
+
+                        if (!(aah_edit_transaction($transaction_id, $edited_transaction))) {
+                            echo "<br><h2>Transaction Edit Failed!</h2>";
+                        } else {
+                            echo "<br><h2>Transaction Successfully Edited!</h2>";
+                        }
+
+                        if ($_POST['search-type'] == 'aid')
+                            $transaction = aah_get_transaction_info_by_aid($_POST['search-key']);
+                        else {
+                            $transaction = aah_get_transaction_info_by_tag($_POST['search-key']);
+                        }
+
+                    }
+
+                    if ($transaction) {
+                        ?>
+                        <br>
+                        <div class="transaction-info-form">
+                            <form action="" method="post" class="transaction-edit-form">
+                                <label>Adoption ID: <a
+                                            href="<?php echo site_url('adoption-information') . "?a_id=" . $transaction['a_id']; ?>"><?php echo $transaction['a_id']; ?></a></label>
+                                <br><label>Tree Tag: <input name="tree_tag" id="tree_tag"
+                                                            value="<?php echo $transaction['tree_tag']; ?>"></label>
+                                <br><label>Name: <input name="name" id="name"
+                                                        value="<?php echo $transaction['name']; ?>"></label>
+                                <br><label>Email: <input name="email" id="email"
+                                                         value="<?php echo $transaction['email']; ?>"></label>
+                                <br><label>Anonymous: <input type="checkbox" name="anonymous" id="anonymous"
+                                                             value="1" <?php
+                                    if ($transaction['anonymous']) {
+                                        echo "checked";
+                                    }
+                                    ?>></label>
+                                <input type="hidden" name="transaction_id" id="transaction_id"
+                                       value="<?php echo $transaction['transaction_id']; ?>">
+                                <input type="hidden" name="search-key" id="search-key"
+                                       value="<?php echo $_POST['search-key']; ?>">
+                                <input type="hidden" name="search-type" id="search-type"
+                                       value="<?php echo $_POST['search-type']; ?>">
+                                <input type="hidden" name="edited" id="edited" value="1">
+                                <br>
+                                <button type="submit" class="transaction-edit-button">Submit Edits</button>
+                            </form>
+                        </div>
+                        <?php
+                    }
+                } ?>
+            </td>
+        </tr>
         <tr>
             <td>
                 <?php
@@ -147,9 +154,8 @@ function aah_render_transaction_lookup()
                 </div>
             </td>
         </tr>
-        </table>
-        <?php
-    }
+    </table>
+    <?php
 }
 
 // edit a given transaction
